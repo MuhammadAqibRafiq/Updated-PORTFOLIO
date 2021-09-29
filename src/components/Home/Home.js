a7535cdc4e6b850e1c0540f4413a70b2fd91ba45
Muhammad Aqib Rafiq <aqibrafiq001@outlook.com>
2021-09-30 00:01:53 +0500

    second

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
           <Col >
             <h3 className='home-title pt-5'>HELLO</h3>
             <h1 className='home-desc'>I am Aqib Rafiq</h1>
-            <h5 className='home-job'>REACT JS DEVELOPER</h5>
+            <h5 className='home-job'>JAMSTACK DEVELOPER</h5>
             <Button className='hire-bt' style={{ marginLeft: "10px" }} href='#Contact'>Hire Me</Button>
             <Button style={{ marginLeft: "10px" }} variant="outline-secondary" href={Pdf} target="_blank" >Get CV</Button>
 
