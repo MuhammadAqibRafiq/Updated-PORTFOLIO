11cd7723077d068f1c24b8f0f360b19f40f7bf6b
Muhammad Aqib Rafiq <aqibrafiq001@outlook.com>
2021-09-29 23:14:03 +0500

    pdf

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container, Row, Col, Button } from "react-bootstrap"
 import AboutLeft from './aboutleft.png'
 import './about.css'
-// import Pdf from './resume.pdf'
+import Pdf from './Resume.pdf'
 
 
 function About() {
@@ -20,9 +20,8 @@ function About() {
                             I describe myself as a passionate developer who loves coding ❤️
                             and ready to work on good project with passion</p>
 
-                        {/* <Button className="abhire-btn" style={{ marginLeft: "10px" }} href={Pdf} target="_blank" >Get CV</Button> */}
+                        <Button className="abhire-btn" style={{ marginLeft: "10px" }} href={Pdf} target="_blank" >Get CV</Button>
 
-                        <Button className="abhire-btn" style={{ marginLeft: "10px" }}  >Get CV</Button>
 
                     </Col>
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomeRight from './home-right.png'
 import { Container, Row, Col, Button } from "react-bootstrap"
 import "./home.css";
-import Pdf from './resume.pdf'
+import Pdf from './Resume.pdf'
 
 export default function Home() {
   return (
@@ -16,7 +16,6 @@ export default function Home() {
             <h5 className='home-job'>REACT JS DEVELOPER</h5>
             <Button className='hire-bt' style={{ marginLeft: "10px" }} href='#Contact'>Hire Me</Button>
             <Button style={{ marginLeft: "10px" }} variant="outline-secondary" href={Pdf} target="_blank" >Get CV</Button>
-            {/* <Button style={{ marginLeft: "10px" }} variant="outline-secondary" >Get CV</Button> */}
 
 
           </Col>
